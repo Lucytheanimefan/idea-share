98844f365ba87b5988d780e8c9ca54afcd71aff2
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-02-11 13:47:08 -0500

    got get request working

--- post.py
+++ post.py
@@ -1,8 +1,10 @@
 import server
 import datetime
 import pymongo
+from JSONEncoder import JSONEncoder
 
 db = server.get_db()
+
 
 def add(title, content, category):
     if (db.ideas.find({"title": title, "content": content}).count() > 0):
@@ -13,6 +15,7 @@ def add(title, content, category):
         return "Success!"
 
 def get(num_posts, category):
+    print "in get"
     if (db.ideas.find({"category": category}).count() > 0):
         doc = db.ideas.find({"category": category}).sort("date", pymongo.DESCENDING)
 
@@ -23,6 +26,9 @@ def get(num_posts, category):
         for p in range(num_posts):
             post = {"title": doc[p]["title"], "content": doc[p]["content"], "category": doc[p]["category"]}
             posts.append(post)
+
+            
+        posts = [JSONEncoder().encode(post) for post in posts]
         return posts
     else:
         return "None"
@@ -30,7 +36,9 @@ def get(num_posts, category):
 def get_date(entry):
     return entry.strftime("%c")
 
-add("new post1", "content", "category")
-array = get(2, "category")
-for i in range(len(array)):
-    print(array[i])
+
+if __name__ == '__main__':
+    add("new post1", "content", "category")
+    array = get(2, "category")
+    for i in range(len(array)):
+        print(array[i])
--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,12 @@
 generateNotes(5);
 
+getNotes({"category":"art", "num_posts":20}, function(d){
+	console.log("populate notes");
+	console.log(d);
+});
 
 function generateNotes(numNotes){
+
 	for (var i=0; i<numNotes; i++){
 		createPostIt();
 	}
@@ -11,3 +16,58 @@ function generateNotes(numNotes){
 function createPostIt(){
 	$("#main").append("<div class='postit'></div>")
 }
+
+
+
+/*---------------------ajax calls-------------------*/
+
+var urlBase = window.location.hostname
+
+function createNote(data) {
+    $.ajax({
+        type: 'POST',
+        url: '/addNote',
+        data: JSON.stringify(data),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'text',
+        success: function(msg, status, jqXHR) {
+            console.log(msg);
+            alert("Submitted data");
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert(textStatus, errorThrown);
+        }
+    });
+}
+
+
+function getNotes(params, callback) {
+    getData("/getNotes", params, callback);
+}
+
+function getData(urlBase=urlBase, params, callback) {
+    var stringifiedParams = jQuery.param(params);
+    url = urlBase + "?" + stringifiedParams;
+    //console.log("Url: " + url);
+    $.getJSON(url, function(json) {
+        console.log("Url: " + url);
+        callback(json);
+    });
+}
+
+/*
+function getNotes(data) {
+    $.ajax({
+        type: 'GET',
+        url: '/getNotes',
+        data: JSON.stringify(data),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'text',
+        success: function(response) {
+           console.log(response);
+           result = response["result"];
+
+        }
+    });
+}
+*/
